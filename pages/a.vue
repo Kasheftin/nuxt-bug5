9a90bca66a301464a294c004db63a0a60023bb2e
Kasheftin <Kasheftin@gmail.com>
2017-09-25 17:49:44 +0300

    Initial commit

---
+++ pages/a.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div style="border:1px dashed #dedede; padding: 10px;">
+      <ul>
+        <li><nuxt-link to="/a/">Go to /a/</nuxt-link></li>
+        <li><nuxt-link to="/a/b/">Go to /a/b/</nuxt-link></li>
+        <li><nuxt-link to="/a/c/">Go to /a/c/</nuxt-link></li>
+      </ul>
+    </div>
+    <div style="border:1px dashed #dedede; padding: 10px;">
+      <nuxt-child/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    console.log('a created')
+  },
+  beforeDestroy () {
+    console.log('a is about to be destroyed')
+  }
+}
+</script>
---
+++ pages/a/b.vue
@@ -0,0 +1,5 @@
+<template>
+  <div style="border:1px solid #dedede; padding: 10px;">
+    <h1>Welcome From B!</h1>
+  </div>
+</template>
